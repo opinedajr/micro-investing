e1210096d6b90f45ac45e9e397c3e918f742a0d5
Jose Pineda <pinedajr@gmail.com>
2026-07-28 18:07:40 -0300

    feat(wallet): register GET routes for wallet List and Find

--- internal/wallet/routes.go
+++ internal/wallet/routes.go
@@ -5,4 +5,6 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(rg *gin.RouterGroup, h *Handler) {
 	wallets := rg.Group("/wallets")
 	wallets.POST("", h.Create)
+	wallets.GET("", h.List)
+	wallets.GET("/:id", h.Find)
 }
