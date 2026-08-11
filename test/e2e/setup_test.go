b817cde036e9964c5290f32ee5e073f89b1f4186
Jose Pineda <pinedajr@gmail.com>
2026-08-10 22:33:18 -0300

    test(e2e): add test-e2e make target and suite setup with sqlite

---
+++ test/e2e/setup_test.go
@@ -0,0 +1,94 @@
+//go:build integration
+
+package e2e
+
+import (
+	"database/sql"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/goleak"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	"github.com/opinedajr/micro-investing/internal/di"
+	"github.com/opinedajr/micro-investing/internal/healthcheck"
+	"github.com/opinedajr/micro-investing/internal/wallet"
+)
+
+
+
+type E2ESuite struct {
+	suite.Suite
+	server    *httptest.Server
+	expect    *httpexpect.Expect
+	db        *sql.DB
+	container *di.Container
+}
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m, goleak.IgnoreTopFunction("net/http.(*Server).Serve"))
+}
+
+func TestE2ESuite(t *testing.T) {
+	suite.Run(t, new(E2ESuite))
+}
+
+func (s *E2ESuite) SetupSuite() {
+	gin.SetMode(gin.ReleaseMode)
+
+	tempDir, err := os.MkdirTemp("", "micro-investing-e2e-*")
+	s.Require().NoError(err)
+	dbPath := filepath.Join(tempDir, "test.db")
+
+	os.Setenv("DB_DRIVER", "sqlite")
+	os.Setenv("DB_NAME", dbPath)
+	os.Setenv("SERVER_PORT", "0")
+
+	s.container = di.NewContainer()
+	
+	db, err := s.container.DB().DB()
+	s.Require().NoError(err, "failed to get underlying sql.DB")
+	s.db = db
+
+	migrationsPath := "file://../../migrations"
+	dbURL := "sqlite3://" + dbPath
+	
+	m, err := migrate.New(migrationsPath, dbURL)
+	s.Require().NoError(err, "failed to initialize migrator")
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		s.Require().NoError(err, "failed to run migrations")
+	}
+
+	_, _ = m.Close()
+
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	healthcheck.RegisterRoutes(v1, s.container.HealthCheckHandler())
+	wallet.RegisterRoutes(v1, s.container.WalletHandler())
+	
+	s.server = httptest.NewServer(r)
+
+	s.expect = httpexpect.Default(s.T(), s.server.URL)
+}
+
+func (s *E2ESuite) TearDownSuite() {
+	s.server.Close()
+	if s.db != nil {
+		s.db.Close()
+	}
+}
+
+func (s *E2ESuite) SetupTest() {
+	err := s.container.DB().Exec("DELETE FROM wallets;").Error
+	s.Require().NoError(err, "falha ao limpar tabela wallets")
+}
